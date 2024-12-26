a97247df242904ecb328cdc6bcd56cb8d0ab019a
Adya Roby <adyaro37@gmail.com>
2024-12-26 22:22:22 +0700

    Update build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -571,7 +571,7 @@ fi
 
 ### Experimental addition to address space hackery
 if [ "$TERMUX_GLIBC" = "true" ]; then
-echo "Applying additional address space patch... (credits to Bylaws)"
+# echo "Applying additional address space patch... (credits to Bylaws)"
 # wget -O address-space.patch https://github.com/bylaws/wine/commit/c12890cafb580764c076e4231636cafaf6e35089.patch
 # patch -p1 < address-space.patch || {
 #        echo "This patch did not apply. Stopping..."
